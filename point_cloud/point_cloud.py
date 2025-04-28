9c3b004f0dbc0924ce24be24aeea30ed9d246347
simgeakgul <simgeakgul20@ku.edu.tr>
2025-04-28 22:56:09 +0300

    set pipes for depth estm.

--- point_cloud/point_cloud.py
+++ point_cloud/point_cloud.py
@@ -1,15 +1,38 @@
-from transformers import pipeline
-from PIL import Image
+from transformers import pipeline, AutoImageProcessor
+import torch
 
-# load pipe
-pipe = pipeline(task="depth-estimation", 
-    model="depth-anything/Depth-Anything-V2-Small-hf",
-    use_fast=False)
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# load image
-image = Image.open("input.jpg")
+processor_depth = AutoImageProcessor.from_pretrained(
+    "depth-anything/Depth-Anything-V2-Large-hf",
+    use_fast=False
+)
+pipe_depth = pipeline(
+    task="depth-estimation", 
+    model="depth-anything/Depth-Anything-V2-Large-hf",
+    image_processor=processor_depth,
+    device=device
+)
 
-# inference
-depth = pipe(image)["depth"]
-print(type(depth))
-depth.save('depth.jpg')
+processor_indoor = AutoImageProcessor.from_pretrained(
+    "depth-anything/Depth-Anything-V2-Metric-Indoor-Large-hf",
+    use_fast=False
+)
+pipe_metric_indoor = pipeline(
+    task="depth-estimation", 
+    model="depth-anything/Depth-Anything-V2-Metric-Indoor-Large-hf",
+    image_processor=processor_indoor,
+    device=device
+)
+
+processor_outdoor = AutoImageProcessor.from_pretrained(
+    "depth-anything/Depth-Anything-V2-Metric-Outdoor-Large-hf",
+    use_fast=False
+)
+pipe_metric_outdoor = pipeline(
+    task="depth-estimation",
+    model="depth-anything/Depth-Anything-V2-Metric-Outdoor-Large-hf",
+    image_processor=processor_outdoor,
+    device=device
+)
+
